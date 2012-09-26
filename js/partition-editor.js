5c0e77411826179a90f87176f20d030b7789e103
Aji Kisworo Mukti <adzy@di.blankon.in>
2012-09-26 10:13:26 +0700

    New: Test for frontend element of partoedi

--- js/partition-editor.js
+++ js/partition-editor.js
@@ -21,8 +21,30 @@ var partoedi = function() {
 		return rect;
 	};
 	
-	var unalocatedPartition = function() {
+	var unalocatedPartition = function(property) {
+		var group = new Kinetic.Group();
 		
+		//var plusIcon = new Kinetic.Image();
+		
+		var rect = new Kinetic.Rect({
+			width: property.width,
+			height: property.height,
+			fill: {
+        start: {
+          x: property.width/2,
+          y: property.width/2
+        },
+        end: {
+          x: 50,
+          y: 50
+        },
+        colorStops: [0, '#A7A2A2', 1, '#DCDCDC']
+      },
+      stroke: 'white',
+      strokeWidth: 2
+		});
+		
+		return rect;
 	};
 	
 	return {
